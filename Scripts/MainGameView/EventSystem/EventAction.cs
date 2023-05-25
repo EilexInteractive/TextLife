afce654b62bde8b2a47a67562b1ed3dee13bcd40
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-25 12:25:12 +1000

    Created a class that we can store actions in perform them

--- Scripts/MainGameView/EventSystem/EventAction.cs
+++ Scripts/MainGameView/EventSystem/EventAction.cs
@@ -1,15 +1,52 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 
 public class EventAction
 {
     public string EventName;
     public LifeEventLog EventLog;
-    public event Action ActionEvent;
-    public event Action<int> ActionEventInt;
-    public event Action<string> ActionEventString;
-    public event Action<float> ActionEventFloat;
-    public event Action<bool> ActionEventBool;
+
+    public event Action EventActionNoProps;
+    public event Action<int> EventActionInt;
+    public event Action<string> EventActionString;
+    public event Action<float> EventActionFloat;
+    public event Action<bool> EventActionBool;
+
+    public void Dispatch<T>(T property)
+    {
+        if(property is string propString)
+        {
+            if(propString == "")
+            {
+                EventActionNoProps?.Invoke();
+            } else 
+            {
+                EventActionString?.Invoke(propString);
+            }
+        } else if(property is int propInt)
+        {
+            EventActionInt?.Invoke(propInt);
+        } else if(property is float propFloat)
+        {
+            EventActionFloat?.Invoke(propFloat);
+        } else if(property is bool propBool)
+        {
+            EventActionBool?.Invoke(propBool);
+        } else 
+        {
+            EventActionNoProps?.Invoke();
+        }
+    }
+
+    public void DispatchAll(string propString, int propInt, float propFloat, bool propBool)
+    {
+        Dispatch<string>("");
+        Dispatch<string>(propString);
+        Dispatch<int>(propInt);
+        Dispatch<float>(propFloat);
+        Dispatch<bool>(propBool);
+    }
 
     public static bool operator ==(string a, EventAction b)
     {
@@ -23,4 +60,6 @@ public class EventAction
     {
         return a != b;
     }
+
+    
 }
