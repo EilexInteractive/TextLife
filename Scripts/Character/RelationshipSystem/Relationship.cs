d089d13764f590f997827df035427a05e4c63cc1
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-05-26 13:04:34 +1000

    Created a class to store the relationships in

---
+++ Scripts/Character/RelationshipSystem/Relationship.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public enum ERelationshipType
+{
+    ACQUAINTANCE = 0,
+    FRIEND = 1,
+    BEST_FRIEND = 2,
+    PARENT,
+    SIBLING
+}
+
+public class Relationship
+{
+    private CharacterDetails _Character_1;
+    public CharacterDetails Character_1 { get => _Character_1; }
+    private CharacterDetails _Character_2;
+    public CharacterDetails Character_2 { get => _Character_2; }
+    private ERelationshipType _RelationshipType;
+    public ERelationshipType RelationshipType { get => _RelationshipType; }
+
+    public Relationship(CharacterDetails a, CharacterDetails b, ERelationshipType type = ERelationshipType.ACQUAINTANCE)
+    {
+        _Character_1 = a;
+        _Character_2 = b;
+        _RelationshipType = type;
+    }
+
+}
